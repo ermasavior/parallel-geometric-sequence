220ed2dcbf08a12c155ad25067872cda608d5505
ilmaalifia <ilma.mahardika@gmail.com>
2019-02-10 21:05:40 +0700

    add duration

--- geosum.c
+++ geosum.c
@@ -1,15 +1,25 @@
+// Compile: mpicc -o geosum geosum.c -lm
+// Run: mpiexec -n <number_of_process> geosum
+
 #include <math.h>
 #include <stdio.h>
 #include <mpi.h>
+#include <time.h>
 
 double count_a(double a, double r, int n);
-
+     
 int main(int argc, char* argv[]) {
     double sum = 0;
     double r = 4;
     double a = 2;
     int i, rank, size;
     double local_a;
+    double max_time = 0;
+
+    clock_t start, end;
+    double cpu_time_used;
+    
+    start = clock();
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -27,10 +37,14 @@ int main(int argc, char* argv[]) {
     }
 
     if (rank == 0) {
-        printf("Total = %f\n", sum);
+        printf("Total = %.0f\n", sum);
     }
 
     MPI_Finalize();
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Duration %f\n", cpu_time_used);
 }
 
 double count_a(double a, double r, int n){
